a658561b0b0e21685402158c3e245088f91e33d0
Gabe Casciano <gabecasciano@gmail.com>
2025-02-09 01:32:06 -0500

    Added the holding registers and reg map

--- FeatherM4_Modbus_Example/FeatherM4_Modbus_Example.ino
+++ FeatherM4_Modbus_Example/FeatherM4_Modbus_Example.ino
@@ -5,6 +5,7 @@
 #include <ArduinoModbus.h>
 
 #define NEO_PIXEL_PIN 8
+#define ENET_SS_PIN 10
 #define ENET_SERVER_PORT 502
 
 #define UID_REG 40001
@@ -30,9 +31,10 @@
 #define COLOR_INPUT_REG RED_INPUT_REG
 #define COLOR_INPUT_REG_LENGTH 3
 
+const uint8_t UID = 1;
 const uint8_t ip_addr[] = {192, 168, 1, 177}; 
 byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+  0x98, 0x76, 0xB6, 0x12, 0xF8, 0xDC
 }; // fix this mac
 IPAddress ip((const uint8_t *)&ip_addr);
 
@@ -51,21 +53,8 @@ void setup() {
   pixel.setBrightness(64);
   pixel.show();
 
-  Ethernet.begin(mac, ip);
-
-  // Check for Ethernet hardware present
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    while (true) {
-      delay(1); // do nothing, no point running without Ethernet hardware
-    }
-  }
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
-  }
-
-  // start the server
-  ethServer.begin();
+  Serial.begin(9600);
+  while(!Serial){ }
 
   // start the Modbus TCP server
   if (!modbusTCPServer.begin()) {
@@ -82,7 +71,35 @@ void setup() {
   modbusTCPServer.configureInputRegisters(BRIGHTNESS_INPUT_REG, 1);
   modbusTCPServer.configureInputRegisters(COLOR_INPUT_REG, COLOR_INPUT_REG_LENGTH);
 
+  modbusTCPServer.holdingRegisterWrite(UID_REG, UID);
+
+  modbusTCPServer.holdingRegisterWrite(MAC_ADDR_0_REG, mac[0]);
+  modbusTCPServer.holdingRegisterWrite(MAC_ADDR_1_REG, mac[1]);
+  modbusTCPServer.holdingRegisterWrite(MAC_ADDR_2_REG, mac[2]);
+  modbusTCPServer.holdingRegisterWrite(MAC_ADDR_3_REG, mac[3]);
+  modbusTCPServer.holdingRegisterWrite(MAC_ADDR_4_REG, mac[4]);
+
+  modbusTCPServer.holdingRegisterWrite(IP_ADDR_0_REG, ip[0]);
+  modbusTCPServer.holdingRegisterWrite(IP_ADDR_1_REG, ip[1]);
+  modbusTCPServer.holdingRegisterWrite(IP_ADDR_2_REG, ip[2]);
+  modbusTCPServer.holdingRegisterWrite(IP_ADDR_3_REG, ip[3]);
+
   modbusTCPServer.inputRegisterWrite(BRIGHTNESS_INPUT_REG, 128);
+
+  Ethernet.init(ENET_SS_PIN)
+  Ethernet.begin(mac, ip);
+
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    return;
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  // start the server
+  ethServer.begin();
 
   pixel.setBrightness(255);
   pixel.show();
